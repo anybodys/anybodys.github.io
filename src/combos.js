0ad9be5caeca1d0d0ca3aa074856b0719e14fb56
kmd <kmd@getgroundgame.com>
2020-03-26 18:33:36 -0700

    Add settings for number of techniques to generate.

--- src/combos.js
+++ src/combos.js
@@ -23,12 +23,11 @@ function loadData(json) {
 }
 
 
-function generate(rank, itemCount) {
+function generate(rank) {
   var beltI = belts.indexOf(rank);
   var selected = []
-  while (selected.length < itemCount) {
+  while (selected.length < localStorage.getItem('itemCount')) {
     var color = belts[Math.floor(Math.random() * beltI)];
-    console.log(color);
     var rand = Math.floor(Math.random() * basicsByBelt[color].length);
     var newBasic = basicsByBelt[color][rand];
     if (selected.indexOf(newBasic) === -1) {
@@ -36,4 +35,31 @@ function generate(rank, itemCount) {
     }
   }
   $('#combo').html(selected.join('<br>'));
+}
+
+
+function setNum() {
+  // Clear current primary.
+  $('#settings #numTechniques .primary').removeClass('primary');
+  // Set new primary.
+  this.classList.add('primary');
+  // Save the settings.
+  localStorage.setItem('itemCount', parseInt(this.textContent));
+}
+
+function setSettings() {
+  var settings = $('#settings');
+
+  // First (and only, atm) setting: number of techinques to generate.
+  var numTechniques = settings.find('#numTechniques');
+  var itemCount = localStorage.getItem('itemCount') || 3;
+  var buttons = numTechniques.find('.button');
+  for (var i = 0; i < buttons.length; i++) {
+    var b = buttons[i];
+    b.onclick = setNum;
+    bNum = parseInt(b.textContent);
+    if (bNum == itemCount) {
+      b.click();
+    }
+  }
 }
